16e7e261cfaf2d669497250ecbc0b217fad0d3b7
Vardhan Hegde <hegdevardhan@gmail.com>
2024-07-31 19:00:39 +0530

    shell script for ec2 linux

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo apt install git -y
+sudo apt install python3 -y
+sudo apt install python-is-python3 -y
+
+sudo apt install python-pip -y
+
+sudo apt install virtualenv -y
+
+virtualenv --python=python3.12 env 
+source env/bin/activate
+
+pip install -r requirements.txt 
+
+flask run
